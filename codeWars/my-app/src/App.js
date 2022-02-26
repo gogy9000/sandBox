349bce7b610a283cc66574b7d85553d29d76512d
gogy9000 <goropashnyj9000@gmail.com>
2022-02-27 00:04:54 +0500

    LessonIII

--- codeWars/my-app/src/App.js
+++ codeWars/my-app/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import LessonI from './lessons/lesson I';
 import LessonII from './lessons/LessonII';
+import LessonIII from './lessons/LessonIII';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <LessonI /> */}
-        <LessonII />
+        {/* <LessonII /> */}
+        <LessonIII />
 
       </header>
     </div>
